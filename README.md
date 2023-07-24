78d0a988c4eb4df921ac0192b0d536a36023ed86
Suraj <snair306@gmail.com>
2023-07-24 09:53:06 -0500

    updating

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# My-Portfolio
+
+# Portfolio
+
+## Description
+A portfolio displaying all my projects and ways to contact me along with my resume! 
+
+## Table of Contents
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Licenses](#licenses)
+- [Contribution](#contribution)
+- [Tests](#test)
+- [Questions](#Questions)
+
+## Installation
+Just go the link here 
+
+## Usage
+If an interested employer is thinking about hiring me.
+
+## Licenses
+[![License](https://img.shields.io/badge/License--green.svg)](https://opensource.org/licenses/)
+
+## Contribution
+Suraj Nair. React. Bootstrap. 
+
+## Tests
+N/A.
+
+## Questions
+Questions can be directed to snair306@gmail.com or https://github.com/ItzBindi
+
+
--- src/index.js
+++ src/index.js
@@ -1,76 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
-
-/*!
-* Start Bootstrap - Freelancer v7.0.7 (https://startbootstrap.com/theme/freelancer)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
-window.addEventListener('DOMContentLoaded', event => {
-
-  // Navbar shrink function
-  var navbarShrink = function () {
-      const navbarCollapsible = document.body.querySelector('#mainNav');
-      if (!navbarCollapsible) {
-          return;
-      }
-      if (window.scrollY === 0) {
-          navbarCollapsible.classList.remove('navbar-shrink')
-      } else {
-          navbarCollapsible.classList.add('navbar-shrink')
-      }
-
-  };
-
-  // Shrink the navbar 
-  navbarShrink();
-
-  // Shrink the navbar when page is scrolled
-  document.addEventListener('scroll', navbarShrink);
-
-  // Activate Bootstrap scrollspy on the main nav element
-  const mainNav = document.body.querySelector('#mainNav');
-  if (mainNav) {
-      new bootstrap.ScrollSpy(document.body, {
-          target: '#mainNav',
-          rootMargin: '0px 0px -40%',
-      });
-  };
-
-  // Collapse responsive navbar when toggler is visible
-  const navbarToggler = document.body.querySelector('.navbar-toggler');
-  const responsiveNavItems = [].slice.call(
-      document.querySelectorAll('#navbarResponsive .nav-link')
-  );
-  responsiveNavItems.map(function (responsiveNavItem) {
-      responsiveNavItem.addEventListener('click', () => {
-          if (window.getComputedStyle(navbarToggler).display !== 'none') {
-              navbarToggler.click();
-          }
-      });
-  });
-
-});
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+0
